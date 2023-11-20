e8bdeec4a319f5c1d601694733fd715232f7a437
Avraj Sahota <underctrl.io@gmail.com>
2023-11-20 13:03:58 +0300

    Initial

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require('dotenv/config');
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+client.on('ready', (client) => {
+    console.log(`${client.user.username} is online.`);
+});
+
+client.login(process.env.TOKEN);
